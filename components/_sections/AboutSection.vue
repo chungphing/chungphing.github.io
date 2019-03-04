51061a7fa70d43ca3e11742f56edcd44217d2490
chungphing <chungphing@gmail.com>
2019-03-04 19:56:41 +0700

    change and minify portrait to fasten fcp

--- components/_sections/AboutSection.vue
+++ components/_sections/AboutSection.vue
@@ -4,11 +4,14 @@
       <h1 class="about-header section-heading">about me</h1>
       <div class="about-card">
         <div class="about-card-portrait">
-            <img src="~/assets/img/portrait.png" alt="portrait" class="about-card-portrait-img">
+            <img src="~/assets/img/portrait.jpg" alt="portrait" class="about-card-portrait-img">
           </div>
         <div class="about-card-detail-text">
           <p>I'm a web developer based in Phnom Penh. I build website for a living and for fun.</p>
           <p>For the nerdy out there, my langauge of choice is Javascript. Although I do know and use PHP and Python, I find myself drawn into Javascript ecosystem more despite the exhausting amount of frameworks and libraries.</p>
+        </div>
+        <div class="button-wrapper">
+          <button id="get-resume">Get My Resume</button>
         </div>
         <SocialPanel/>
       </div>
@@ -56,6 +59,9 @@ export default {
 .about-card-portrait {
   width: 120px;
   margin: 20px auto;
+}
+.about-card-portrait img {
+  border-radius: 100%;
 }
 </style>
 
